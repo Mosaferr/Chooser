ac81b727082b3b8e8b87d16824c4ed88e0688d6f
Mosafer <grzech.kowalczyk@gmail.com>
2025-05-13 20:26:11 +0200

    1. Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from './employee.model'; // Zakładając, że model jest w pliku employee.model.ts
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  employees: Employee[] = [];
+  selectedEmployeeId: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Employee[]>('assets/data/employees.json')
+      .subscribe(data => {
+        this.employees = data;
+      });
+  }
 }
